242e91bbcaf7f1a7f48fb35df794ae50a4380179
Leejinuk <leejin7900@gmail.com>
2022-10-20 23:22:52 +0900

    com13

--- pages/Fragment.js
+++ pages/Fragment.js
@@ -7,10 +7,16 @@ export default function Fragment() {
     <div className={styles.subsite}>
       <p>Fragments 테스트</p>
       <p>Hello world!</p>
-      <Image src="/frag.png" width={1000} height={354} />
+      <Image src="/black.gif" width={480} height={270} />
+      <br />
       <a href="https://projectlog.tistory.com/14" className={styles.blog}>
         [나의 블로그 글보기]
       </a>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Image src="/frag.png" width={1000} height={354} />
     </div>
   );
 }

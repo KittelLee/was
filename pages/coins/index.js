fdbb2fd7133e9a79bea0cd0a613da57013c6111e
Leejinuk <leejin7900@gmail.com>
2022-10-20 22:56:41 +0900

    com12

--- pages/coins/index.js
+++ pages/coins/index.js
@@ -1,30 +1,33 @@
 import axios from "axios";
-import styles from "../../styles/coin.module.css";
-import Coin from "./Coin";
+import styles from "../../styles/Home.module.css";
 
 export default function CoinList({ coinData }) {
+  console.log(coinData);
   const coins = coinData.coins;
   return (
-    <div className={styles.container}>
+    <div className={styles.subsite}>
       <h1>Coin List </h1>
-      <div className={styles.coinContainer}>
-        {coins.map((coin) => {
-          return (
-            <div key={coin.id} className={styles.coinItem}>
-              <Coin coin={coin} />
-            </div>
-          );
-        })}
-      </div>
+      {coins.map((coin) => {
+        return (
+          // eslint-disable-next-line react/jsx-key
+          <div key={coin.id}>
+            <img src={coin.icon} width={100} height={100} />
+            <h3>
+              {coin.name} - {coin.symbol} - {coin.price}
+            </h3>
+            <p>availableSupply: {coin.availableSupply}</p>
+            <p>contractAddress: {coin.contractAddress}</p>
+            <a href={coin.websiteUrl}> Website: {coin.websiteUrl}</a>
+          </div>
+        );
+      })}
     </div>
   );
 }
-
 export const getStaticProps = async () => {
   const result = await axios.get(
     "https://api.coinstats.app/public/v1/coins?skip=0"
   );
-
   return {
     props: {
       coinData: result.data,
